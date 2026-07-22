9a5bb215924dd9197dfac6c061c9802411da5bf3
Vanshika Ramoshi <vanshikaramoshi664@gmail.com>
2026-07-22 12:39:50 +0530

    Add CalculatorMain class for demonstration

---
+++ Polymorphism/Calculator/CalculatorMain.java
@@ -0,0 +1,13 @@
+package com.polymorphism;
+
+public class CalculatorMain {
+ 
+	public static void main(String[] args) {
+		 
+		Calculator c1 = new Calculator ();
+		c1.add(2, 3);
+		c1.add(2, 3, 2);
+		
+	}
+		
+}

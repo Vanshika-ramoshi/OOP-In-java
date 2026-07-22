e7d0cd4a044dd1707beb191d56d2e42cb558c141
Vanshika Ramoshi <vanshikaramoshi664@gmail.com>
2026-07-22 14:11:41 +0530

    Update PracticeMain.java

--- Polymorphism/Practice/PracticeMain.java
+++ Polymorphism/Practice/PracticeMain.java
@@ -7,7 +7,7 @@ public class PracticeMain {
 	public static void main(String[] args) {
 		 
 		Scanner sc = new Scanner(System.in);
-    Practice p = new Practice ();	 
+        Practice p = new Practice ();	 
     
 		System.out.println("Enter name");
 		String name = sc.nextLine();
@@ -17,5 +17,37 @@ public class PracticeMain {
 		int number = sc.nextInt();
 		p.display(number);
     
+	}
+}
+---------------------------------------------------------------------------------------------
+package com.polymorphism;
+
+import java.util.Scanner;
+
+public class PracticeMain {
+
+	public static void main(String[] args) {
+		 
+		Scanner sc = new Scanner(System.in);
+        Practice p = new Practice ();	
+	
+	
+	    System.out.println("Enter number ");
+		int num = sc.nextInt();
+		sc.nextLine();
+		
+		System.out.println("Enter Name");
+		String name = sc.nextLine();
+		
+		p.show(num, name);
+		sc.nextLine();
+		System.out.println("Enter name ");
+		String Name = sc.nextLine();
+		
+		System.out.println("Enter num");
+		int number = sc.nextInt();
+		
+		p.show(name, num);
+		
 	}
 }

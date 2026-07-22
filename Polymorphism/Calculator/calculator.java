1761972d5d9087be2509f14f46445d816d939d53
Vanshika Ramoshi <vanshikaramoshi664@gmail.com>
2026-07-22 12:38:57 +0530

    Create calculator.java

---
+++ Polymorphism/Calculator/calculator.java
@@ -0,0 +1,15 @@
+package com.polymorphism;
+
+public class Calculator {
+	
+	public void add(int a ,int b) {
+	int sum = a + b;
+	System.out.println("Addition of two numbers : "+ sum);
+	}
+	
+	public void add( int a , int b, int c) {
+		int sum = a + b + c;
+		System.out.println("Addition of three numbers : "+ sum);
+		
+	}
+}

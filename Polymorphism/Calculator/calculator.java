a813a152081964adb825a16afc124d8f1a9e6371
Vanshika Ramoshi <vanshikaramoshi664@gmail.com>
2026-07-22 12:50:30 +0530

    Update calculator.java

--- Polymorphism/Calculator/calculator.java
+++ Polymorphism/Calculator/calculator.java
@@ -10,6 +10,15 @@ public class Calculator {
 	public void add( int a , int b, int c) {
 		int sum = a + b + c;
 		System.out.println("Addition of three numbers : "+ sum);
+	}
+
+	public void multiply(int a , int b) {
+		int sum = a * b;
+		System.out.println("Multiplication of two number : "+ sum);
 		
+	}
+	public void multiply(double a, double b) {
+		double sum = a * b;
+		System.out.println("Multiplication of two number :"+ sum);
 	}
 }

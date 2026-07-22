768c6b239fff6c9ff3a8d9d7aa42e0bb96d12a5a
Vanshika Ramoshi <vanshikaramoshi664@gmail.com>
2026-07-22 13:40:07 +0530

    Add Practice class with overloaded display methods

---
+++ Polymorphism/Practice/Practice.java
@@ -0,0 +1,15 @@
+package com.polymorphism;
+
+public class Practice {
+
+	public void display(String name) {
+		System.out.println("String value :" + name);
+		System.out.println("-------------------------------------");
+
+	}
+
+	public void display(int number) {
+		System.out.println("Integer value :" + number);
+
+	}
+}
